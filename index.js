cda2b33f7e7b6c1dba0949423bafc3e11a0d16d6
Matthew <mattjhlee25@gmail.com>
2023-05-05 15:05:07 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    const formData = {
+        name: `${name}`,
+        email: `${email}`,
+    }
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    }
+    
+    return fetch ("http://localhost:3000/users", configObj)
+        .then( (resp) => resp.json())
+        .then( (data) => document.body.textContent = `${data.id}`)
+        .catch( (error) => {
+            alert("BAD BAD BAD")
+            document.body.textContent = `${error.message}`
+        })
+}
